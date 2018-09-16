bac2eb55ba0673350537ed07f7fc4ca4f3b19f61
Julien Donck <donckjulien@gmail.com>
2018-09-16 16:12:31 +0200

    made reusable modal, portal & toggle components

---
+++ src/utils/Modal.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
+import Portal from './Portal';
+import colors from './colors'
+
+export default class Modal extends Component {
+  render() {
+    const { children, toggle, on } = this.props;
+    return (
+      <Portal>
+        {on &&
+          <ModalWrapper>
+            <ModalCard>
+
+            {/* <CloseButton onClick={toggle}>close</CloseButton> */}
+            <div>{children}</div>
+          </ModalCard>
+          <Background onClick={toggle}/>
+          </ModalWrapper>
+        }
+      </Portal>
+    )
+  }
+}
+
+const ModalWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  font-family: sans-serif;
+  font-size: 16px;
+  color: ${colors.black};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const ModalCard = styled.div`
+  position: relative;
+  background: white;
+  padding: 24px;
+  min-width: 480px;
+  z-index: 10;
+  -webkit-box-shadow: 0px 4px 16px rgba(51, 51, 51, 0.08);
+  -moz-box-shadow: 0px 4px 16px rgba(51, 51, 51, 0.08);
+  box-shadow: 0px 4px 16px rgba(51, 51, 51, 0.08);
+  -webkit-border-radius: 4px;
+  -moz-border-radius: 4px;
+  border-radius: 4px;
+`
+const Background = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: black;
+  opacity: .3;
+`
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,9 @@
 import colors from './colors';
 import lightenDarken from './lightenDarken';
+import Portal from './Portal';
+import Modal from './Modal';
+import Toggle from './Toggle';
 
 export * from './colors';
 
-export { colors, lightenDarken };
+export { colors, lightenDarken, Portal, Modal, Toggle };
