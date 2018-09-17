98294e7fb414af06136a372fce94e762bf57e8a4
Julien Donck <donckjulien@gmail.com>
2018-09-18 00:11:31 +0200

    making demo label pink

--- src/utils/labelParser.js
+++ src/utils/labelParser.js
@@ -3,21 +3,29 @@ import styled from 'styled-components';
 import colors from './colors';
 
 export default (desc) => {
-  const parts = desc.split(/\[(.*?)\]/);
-  for (var i = 1; i < parts.length; i += 2) {
-    parts[i] = <Label key={i}>{parts[i]}</Label>
+  if (desc.includes('[demo]')) {
+    const parts = desc.split(/\[(.*?)\]/);
+    for (var i = 1; i < parts.length; i += 2) {
+      parts[i] = <Label pink key={i}>{parts[i]}</Label>
+    }
+    return <Fragment>{parts}</Fragment>;
+  } else {
+    const parts = desc.split(/\[(.*?)\]/);
+    for (var i = 1; i < parts.length; i += 2) {
+      parts[i] = <Label key={i}>{parts[i]}</Label>
+    }
+    return <Fragment>{parts}</Fragment>;
   }
-  return <Fragment>{parts}</Fragment>;
 }
 
 const Label = styled.span`
-  background-color: ${colors.yellow};
+  background-color: ${props => props.pink ? colors.pink : colors.yellow};
   display:inline-block;
   padding: 4px;
   -webkit-border-radius: 2px;
   -moz-border-radius: 2px;
   border-radius: 2px;
-  color: ${colors.black};
+  color: ${props => props.pink ? colors.white : colors.black};
   font-family: monospace;
   font-size: 14px;
   margin-left: 4px;
