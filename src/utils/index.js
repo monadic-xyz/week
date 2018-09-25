e0b10389a1a88fa7c5b9198b9a20207e0ea1f232
Julien Donck <donckjulien@gmail.com>
2018-09-25 11:20:47 +0200

    adding media queries to utilities

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 import colors from './colors';
+import media from './media-templates';
 import labelParser from './labelParser';
 import Portal from './Portal';
 import Modal from './Modal';
@@ -6,4 +7,4 @@ import Toggle from './Toggle';
 
 export * from './colors';
 
-export { colors, labelParser, Portal, Modal, Toggle };
+export { colors, labelParser, Portal, Modal, Toggle, media };
---
+++ src/utils/media-templates.js
@@ -0,0 +1,23 @@
+import { css } from 'styled-components'
+
+const sizes = {
+  extraWide: 1640,
+  wide: 1440,
+  desktop: 1060,
+  tablet: 768,
+  phone: 420,
+}
+
+// Iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)}
+    }
+  `
+
+  return acc
+}, {})
+
+
+export default media;
