b1abbf234ab44b23cfc0e86adefa429ff90409e8
Julien Donck <donckjulien@gmail.com>
2018-10-29 17:39:01 +0100

    oops

--- src/elements/Icon.js
+++ src/elements/Icon.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { colors } from '../styles';
+import { colors } from 'styles';
 
 export default class Icon extends Component {
   static defaultProps = {
