c29809b9278c7a96b575eb1ede24a3d0b812c697
Julien Donck <donckjulien@gmail.com>
2018-10-30 11:35:30 +0100

    added week number

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -39,6 +39,28 @@ const parseFilter = search => {
   };
 };
 
+const getWeek = date => {
+  const time = new Date(date.getTime());
+
+  time.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  time.setDate(time.getDate() + 3 - ((time.getDay() + 6) % 7));
+  // January 4 is always in week 1.
+  const week1 = new Date(time.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from time to week1.
+  return (
+    1 +
+    Math.round(
+      ((time.getTime() - week1.getTime()) / 86400000 -
+        3 +
+        ((week1.getDay() + 6) % 7)) /
+        7
+    )
+  );
+};
+
+const today = new Date();
+
 export default class Tasks extends Component {
   static propTypes = {
     history: PropTypes.shape({
@@ -92,7 +114,7 @@ export default class Tasks extends Component {
     return (
       <>
         <Header>
-          <SegmentTitle>{segment}</SegmentTitle>
+          <SegmentTitle>Week {getWeek(today)}</SegmentTitle>
           <SegmentToggle />
           <Search onSubmit={this.search} term={filter.query || ''} />
         </Header>
