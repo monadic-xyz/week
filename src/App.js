4a49b8d79751f04feef11ea82f18797cc5154480
Julien Donck <donckjulien@gmail.com>
2018-10-30 17:50:49 +0100

    automatic redirect to /tasks/open

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
 import Tasks from 'pages/Tasks';
 
 export default () => (
   <Router>
-    <Route path="/tasks/:segment(archived|open)" component={Tasks} />
+    <Switch>
+      <Route exact path="/" render={() => <Redirect to="/tasks/open" />} />
+      <Route path="/tasks/:segment(archived|open)" component={Tasks} />
+    </Switch>
   </Router>
 );
