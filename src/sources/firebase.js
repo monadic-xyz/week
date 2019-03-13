bf7ed16df70e05dd1c5e4803279cbc8dc53c120e
Alexander Simmerl <a.simmerl@gmail.com>
2019-03-13 13:03:31 +0100

    Remove API keys

--- src/sources/firebase.js
+++ src/sources/firebase.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 
 let config = {
-  apiKey: 'AIzaSyAEFLMrNMHgiuof4yWnUVBL6a6ZbLM-GQA',
+  apiKey: 'PUT_YOUR_API_KEY_HERE',
   authDomain: 'week-planner-dev.firebaseapp.com',
   databaseURL: 'https://week-planner-dev.firebaseio.com',
   projectId: 'week-planner-dev',
@@ -12,7 +12,7 @@ let config = {
 
 if (process.env.NODE_ENV === 'production') {
   config = {
-    apiKey: 'AIzaSyDeSjVLasCiUsKwC1TPWsTjKvhz-FSQkCY',
+    apiKey: 'PUT_YOUR_API_KEY_HERE',
     authDomain: 'week-planner-b8136.firebaseapp.com',
     databaseURL: 'https://week-planner-b8136.firebaseio.com',
     projectId: 'week-planner-b8136',
